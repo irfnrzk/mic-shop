570c7406b584170e44df675ee3bc3b62bd55bf97
Muhammad Irfan Ab Razak <irfan.abrazak@gmail.com>
2020-07-30 16:00:48 +0800

    getServerSideProps with nprogress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,60 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from './theme';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Box from '@material-ui/core/Box'
-import { Container } from '@material-ui/core';
+// import App from 'next/app'
+import { Router } from 'next/dist/client/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
+// NProgress.start()
+// NProgress.done()
+NProgress.configure({ showSpinner: false });
 
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
+Router.events.on('routeChangeStart', () => {
+  NProgress.start()
+})
 
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done()
+})
+
+Router.events.on('routeChangeError', () => {
+  NProgress.done()
+})
+
+function MyApp({ Component, pageProps }) {
   return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <AppBar position="fixed">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" >
-            Microphone Shop
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Container>
-          <Box marginTop={10}>
-            <Component {...pageProps} />
-          </Box>
-        </Container>
-      </ThemeProvider>
-    </React.Fragment>
-  );
+    <Component {...pageProps} />
+  )
 }
 
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,8 @@
 import { Microphone } from "../../model/Microphone";
-import { GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 import { openDB } from "../openDB";
-import Link from 'next/link';
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { Grid } from "@material-ui/core";
+import { MicrophoneDetail } from "./microphone/[id]";
+import Link from "next/link";
 
 export interface IndexProps {
   microphones: Microphone[];
@@ -19,51 +10,22 @@ export interface IndexProps {
 
 export default function Index({ microphones }: IndexProps) {
   return (
-    <Grid container spacing={3}>
-      {microphones.map(mic => {
-        return (
-          <Grid item xs={12} sm={3} key={mic.id}>
-            <Link href="/microphone/[id]" as={`/microphone/${mic.id}`}>
-              <a>
-                <Card>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      alt={mic.model + ' ' + mic.brand}
-                      height="300"
-                      image={mic.imageUrl}
-                      title={mic.model + ' ' + mic.brand}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {mic.brand + ' ' + mic.model}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary" component="p">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
-                    </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </a>
-            </Link>
-          </Grid>
-        )
-      })}
-    </Grid>
+    <div>
+      <Link href="/people">
+        <a>People</a>
+      </Link>
+      <pre>{JSON.stringify(microphones, null, 4)}</pre>
+    </div>
   )
 }
 
-export const getStaticProps: GetStaticProps = async ctx => {
-  const currentPage = ctx.params?.currentPage as string;
-  const currentPageNumber = +(currentPage || 0);
-
-  const min = currentPageNumber * 5;
-  const max = (currentPageNumber + 1) * 5;
-
-
+export const getServerSideProps: GetServerSideProps<IndexProps> = async ctx => {
   const db = await openDB();
-  const microphones = await db.all('select * from microphone where id > ? and id <= ?', min, max);
+  const microphones = await db.all<Microphone[]>('select * from microphone');
+
+  await new Promise(acc => {
+    setTimeout(acc, 3000)
+  });
 
   return { props: { microphones } };
 }
